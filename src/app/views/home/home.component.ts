65d6c71e88948151266b2b87df278e392d23a69d
flond7 <elisa.utilivenza@gmail.com>
2021-03-08 10:02:19 +0100

    chantes to home to test components

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  rows = [
+    {"first_name":"Elisa","last_name":"Pessa","year_id": 1994, "paid": true},
+    {"first_name":"Elisa","last_name":"Pessa","year_id": 1994, "paid": true},
+    {"first_name":"Elisa","last_name":"Pessa","year_id": 1994, "paid": false}
+  ]
   constructor() { }
 
   ngOnInit(): void {
