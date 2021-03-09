afdc76a0828d6a53cc6f67247b8435932e38e9fb
flond7 <elisa.utilivenza@gmail.com>
2021-03-09 11:20:10 +0100

    added list routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { ListDrainComponent } from './views/list-drain/list-drain.component';
+import { ListUsersComponent } from './views/list-users/list-users.component';
+import { ListYearsComponent } from './views/list-years/list-years.component';
 import { TestViewComponent } from './views/test-view/test-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'test-view', component: TestViewComponent }
+  { path: 'list-drain', component: ListDrainComponent },
+  { path: 'list-user', component: ListUsersComponent },
+  { path: 'list-year', component: ListYearsComponent },
+  { path: 'test-view', component: TestViewComponent },
 ];
 
 @NgModule({
