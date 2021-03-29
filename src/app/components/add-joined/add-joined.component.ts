c581fe3e46a450fb3dd97265867fec06fd94310a
flond7 <elisa.utilivenza@gmail.com>
2021-03-29 12:29:55 +0200

    added realtime messages in joined component

--- src/app/components/add-joined/add-joined.component.ts
+++ src/app/components/add-joined/add-joined.component.ts
@@ -11,15 +11,37 @@ export class AddJoinedComponent implements OnInit {
   errorYear = false;
   errorUser = false;
   errorDrain = false;
-  data: any;
+  year: any;
+  cf: any;
+  num: any;
 
   constructor(public api: ApiService) { }
 
   ngOnInit(): void {
   }
 
-  searchYear(year:any){
-    this.api.findSingleYear(year).subscribe(res =>{console.log(res)})
+  searchYear(){
+    this.errorYear = false;
+    this.api.findSingleYear(this.year)
+    .subscribe(
+      (res) => { console.log(res) },
+      (error) => { this.errorYear = true;})
+  }
+
+  searchUser(){
+    this.errorUser = false;
+    this.api.findSingleUser(this.cf)
+    .subscribe(
+      (res) => { console.log(res) },
+      (error) => { this.errorUser = true;})
+  }
+
+  searchDrain(){
+    this.errorDrain = false;
+    this.api.findSingleDrain(this.cf)
+    .subscribe(
+      (res) => { console.log(res) },
+      (error) => { this.errorDrain = true;})
   }
 
 }
