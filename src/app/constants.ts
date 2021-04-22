d37ae78f6add7c6195dddc8e37abd7ff93aaba06
flond7 <elisa.utilivenza@gmail.com>
2021-04-22 10:03:20 +0200

    cleanup

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,14 +1,3 @@
-/* export const FORM_ERR_MESG : { [key: string]: any } = {
-  saved: "Elemento salvato",
-  warning: "Compila tutti i campi",
-  dangerYear: "Errore: probabilmente hai già inserito i dati per quest'anno",
-  dangerDrain: "Errore: probabilmente hai già inserito i dati per questa canaletta",
-  dangerUser: "Errore: probabilmente hai già inserito i dati per quest'utente",
-  email: "Inserisci una mail valida",
-  required: "Campo obbligatorio"
-} as const;
- */
-
 export class GlobalConstants {
 
   public static FORM_ERR_MESG : { [key: string]: any } = {
