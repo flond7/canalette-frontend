0ee12387873d68854d80caf2da3cbae655ed88c2
flond7 <elisa.utilivenza@gmail.com>
2021-04-29 12:09:35 +0200

    add single year working

--- src/app/components/anag-year/anag-year.component.ts
+++ src/app/components/anag-year/anag-year.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
+import * as GB from "src/app/constants";
 
 @Component({
   selector: 'app-anag-year',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnagYearComponent implements OnInit {
 
-  constructor() { }
+  @Input() status: string = 'new'; //new | edit | view
+  warningMessage = "";
+  required="";
+  type = "success";
+  showAlert = false;
+  data = { "year":"","taxCitizen":"","taxBusiness":""};
+  
+  constructor(public api: ApiService, public fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  /* REACTIVE FORM DRAIN */
+  yearForm = new FormGroup({
+    year: new FormControl('', [Validators.required]),
+    taxBusiness: new FormControl('', [Validators.required]),
+    taxCitizen: new FormControl('', [Validators.required])
+  });
+  get fy(){return this.yearForm.controls;}
+
+  ngOnInit(): void { 
+    this.required = GB.GlobalConstants.FORM_ERR_MESG.required;
+  }
+
+  saveDrain() {
+    let year = {
+      "year": this.data.year,
+      "taxBusiness": this.data.taxBusiness,
+      "taxCitizen": this.data.taxCitizen
+    };
+
+    this.api.createYear(year).subscribe(
+      (res) => { 
+        this.warningMessage = GB.GlobalConstants.FORM_ERR_MESG.saved; 
+        this.type = 'success', 
+        this.showAlert = true
+        this.reset();
+      },
+      (error) => { 
+        this.warningMessage = GB.GlobalConstants.FORM_ERR_MESG.dangerYear; 
+        this.type = 'danger', 
+        this.showAlert = true}
+    );
+  }
+
+  reset() {
+    this.data = {"year":"","taxCitizen":"","taxBusiness":""};
+    this.yearForm.markAsUntouched();
+    this.yearForm.markAsPristine();
   }
 
 }
