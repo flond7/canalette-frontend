51d85977d682830b53d01b05ddcec600d1cfe9b9
flond7 <elisa.utilivenza@gmail.com>
2021-05-07 08:27:55 +0200

    managing errors

--- src/app/components/bulk-year/bulk-year.component.ts
+++ src/app/components/bulk-year/bulk-year.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { NgxCsvParser } from 'ngx-csv-parser';
 import { NgxCSVParserError } from 'ngx-csv-parser';
@@ -21,11 +21,28 @@ export class BulkYearComponent implements OnInit {
   everythingOk = 0;
   uploadLen: any;
 
+  @Input() uploadTab: string = 'drain';   //year | user | drain | relation
+
   constructor(public api: ApiService, private ngxCsvParser: NgxCsvParser) { }
 
   ngOnInit(): void {
   }
 
+  ifRes() {
+    this.everythingOk++;
+    if (this.everythingOk == this.uploadLen) {
+      console.log("OK");
+      this.warningMessage = GB.GlobalConstants.FORM_ERR_MESG.saved;
+      this.type = 'success';
+      this.showAlert = true;
+    }
+  }
+
+  ifErr() {
+    this.type = 'danger';
+    this.showAlert = true;
+    this.warningMessage = "Sono stati inseriti solo " + this.everythingOk + " record. Controlla il file excel e che i dati per questi anni non si trovino già nel database"
+  }
   splitAndFormObject(elem: any, index: any) {
     let elements = elem.split(';');
     let obj: any = {};
@@ -33,21 +50,23 @@ export class BulkYearComponent implements OnInit {
       let a = index[i];
       obj[a] = elements[i];
     }
-    //push in array of objects
-    //this.csvData.push(obj)
-    this.api.createYear(obj).subscribe(
-      (res) => { 
-        this.everythingOk++;
-        if (this.everythingOk == this.uploadLen) {
-        console.log("OK");
-        this.warningMessage = GB.GlobalConstants.FORM_ERR_MESG.saved;
-        this.type = 'success';
-        this.showAlert = true;
-      }; },
-      (error) => { 
-        this.type = 'danger'; 
-        this.showAlert = true;
-        this.warningMessage = "Sono stati inseriti solo " + this.everythingOk + " record. Controlla il file excel e che i dati per questi anni non si trovino già nel database" })
+    if (this.uploadTab == "year") {
+      this.api.createYear(obj).subscribe(
+        (res) => { this.ifRes() },
+        (error) => { this.ifErr() })
+    } else if (this.uploadTab == "drain") {
+      this.api.createDrain(obj).subscribe(
+        (res) => { this.ifRes() },
+        (error) => { this.ifErr() })
+    } else if (this.uploadTab == "relation") {
+      this.api.createRelational(obj).subscribe(
+        (res) => { this.ifRes() },
+        (error) => { this.ifErr() })
+    } else {
+      this.api.createUser(obj).subscribe(
+        (res) => { this.ifRes() },
+        (error) => { this.ifErr() })
+    }
   }
 
   // Your applications input change listener for the CSV File
