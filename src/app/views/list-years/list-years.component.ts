0ee92b66c4ed167fbcf68bbb65dff2170056707b
flond7 <elisa.utilivenza@gmail.com>
2021-05-07 08:25:54 +0200

    basic list years

--- src/app/views/list-years/list-years.component.ts
+++ src/app/views/list-years/list-years.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+
+/* FA ICONS */
+import { faCalendar, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-list-years',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-years.component.sass']
 })
 export class ListYearsComponent implements OnInit {
+  faCalendar = faCalendar;
+  dataResult: any;
+  noData = false;
 
-  constructor() { }
+  listOfColumns = {
+    "first_name": true,
+    "last_name": true,
+    "year": true,
+    "drain": true,
+    "paid": true,
+    "amount_computed": true,
+    "amount_paid": true,
+    "bill_number":true
+  };
+
+  constructor(public api: ApiService) { }
 
   ngOnInit(): void {
+  }
+  searchListResult(e: any) {
+    this.dataResult = e;
+    //if searched data is not assigned to joined table and I've never done this check before
+    if (this.dataResult[0].id == null && this.noData == false) {
+      console.log("no data")
+      this.noData = true;
+    }
   }
 
 }
