783bbb768247a341bb04866e6e4aefe61d0ce5d7
flond7 <elisa.utilivenza@gmail.com>
2021-05-31 12:04:08 +0200

    label changes

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -11,7 +11,7 @@ export class GlobalConstants {
     joinedErrYear: "Le tariffe per quest'anno non sono ancora state definite. Inseriscile prima di proseguire",
     joinedErrUser: "Questo utente non si trova nel sistema",
     joinedErrDrain: "Questa canaletta non è stata inserita nel sistema",
-    joinedErr: "Questa relazione è giàa stata inserita"
+    joinedErr: "Questa relazione è già stata inserita"
   }
   
 
